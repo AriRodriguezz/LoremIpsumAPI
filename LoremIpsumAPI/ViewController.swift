4cbac26d6a7108dc6182b11c23c3f9f063e7ed10
ARI RODRIGUEZ-CERVANTES <rl200288@rlas-macair-15.rlas-116.org>
2018-11-30 12:41:05 -0600

    Initial Commit

---
+++ LoremIpsumAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoremIpsumAPI
+//
+//  Created by ARI RODRIGUEZ-CERVANTES on 11/30/18.
+//  Copyright © 2018 ARI RODRIGUEZ-CERVANTES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
