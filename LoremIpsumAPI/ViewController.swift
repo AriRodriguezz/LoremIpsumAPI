230d6964b1330bc199ff8f71bc5017cf2562da9f
ARI RODRIGUEZ-CERVANTES <rl200288@rlas-macair-15.rlas-116.org>
2018-12-20 09:13:18 -0600

    added textView

--- LoremIpsumAPI/ViewController.swift
+++ LoremIpsumAPI/ViewController.swift
@@ -6,15 +6,63 @@
 //  Copyright © 2018 ARI RODRIGUEZ-CERVANTES. All rights reserved.
 //
 
+struct Images: Codable {
+    let author: String
+    let postUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case author
+        case postUrl = "post_url"
+    }
+}
+
 import UIKit
-
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var UIImageView: UIImageView!
+    @IBOutlet weak var AuthorLabel: UILabel!
+    
+    let jsonUrlString = "https://picsum.photos/list"
+    var imageURL = String()
+    var author = String()
+    var imageArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        parse()
     }
-
-
+    @IBAction func PictureButton(_ sender: Any) {
+        let randomNumber = Int.random(in: 0 ... 992)
+        parse(number: randomNumber)
+    }
+    
+    func parse(number: Int){
+        guard let url = URL(string: jsonUrlString) else { return }
+        URLSession.shared.dataTask(with: url){ (data, response, err) in
+            guard let data = data else {return}
+            do {
+                let images = try JSONDecoder().decode([Images].self, from: data)
+                //                print(images)
+                for image in images {
+                    self.imageArray.append(image.postUrl)
+                    self.author = image.author
+                    self.imageURL = image.postUrl
+//                    print(self.author)
+//                    print(self.imageURL)
+                }
+            } catch let jsonErr {
+                print("error serializing data:", jsonErr )
+            }
+//            print(self.imageArray.count)
+//            print(self.imageArray.endIndex)
+            print(self.imageArray[number])
+            self.imageURL = self.imageArray[number]
+            }.resume()
+        
+        UrlLabel.text = imageURL
+    }
+    
+    
+    
 }
 
